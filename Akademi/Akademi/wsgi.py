c1985f1e1192e6a9661f09aebaed3de899f6f086
Natan Rincon Luna <94479333+natanrinconluna@users.noreply.github.com>
2025-03-29 23:10:40 -0700

    changes wsgi

--- Akademi/Akademi/wsgi.py
+++ Akademi/Akademi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Akademi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Akademi.Akademi.settings')
 
 application = get_wsgi_application()
