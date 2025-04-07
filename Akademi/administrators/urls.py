99ef34631b45ea4a1fcd17c98ef9abf4dbe12178
kacylam <kacy.lam04@gmail.com>
2025-04-07 00:03:40 -0700

    update for admin-professor pages

--- Akademi/administrators/urls.py
+++ Akademi/administrators/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('semester/create/', views.create_semester, name='create_semester'),
 
     path('manage_courses/list/', views.admin_courses_manage, name='admin_courses_manage'),
-
-    path('professors/', views.admin_professors, name='admin_professors'),
+    path('manage_professors/list/', views.admin_professors_manage, name='admin_professors_manage'),
     path('students/', views.admin_students, name='admin_students'),
 
     # Admin course create (corrected)
@@ -21,8 +20,13 @@ urlpatterns = [
     path('manage_courses/<int:course_id>/edit/', views.edit_course, name='edit_course'),
     path('manage_courses/<int:course_id>/delete/', views.delete_course, name='delete_course'),
 
-    # Admin professor and student create paths
-    path('professors_create/', views.admin_professors_create, name='admin_professors_create'),
+    # Admin professor paths
+    path('professors_create', views.create_professor, name='create_professor'),
+    path('manage_professors/<int:professor_id>/', views.professor_detail, name='professor_detail'),
+    path('manage_professors/<int:professor_id>/edit/', views.edit_professor, name='edit_professor'),
+    path('manage_professors/<int:professor_id>/delete/', views.delete_professor, name='delete_professor'),
+
+    # Admin student
     path('students_create/', views.admin_students_create, name='admin_students_create'),
 
     path('logout/', views.administrators_logout, name='logout'),
